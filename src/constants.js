6d900709d594a0a604a190e152ffe772814b5c60
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-09-29 15:47:57 +0300

    CUB-P2
    CUB-P2, exctract connection string in the config, connect app to the db

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const constants = {
     PORT: 5050,
+    URL: "mongodb://127.0.0.1/cubicle-sep-20233" // connection string 
 }
 
 module.exports = constants;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 //imports
 
 const express = require("express");
+
 const handlebarsConfig = require("./config/handlebarsConfig");
 const expressConfig = require("./config/expressConfig");
+const dbConnect = require("./config/dbConfig");
+
 const { PORT } = require("./constants.js");
 const routes = require("./router");
+
 
 // local variables
 const app = express();
@@ -13,9 +17,13 @@ const app = express();
 handlebarsConfig(app);
 expressConfig(app);
 
+//connecting to the DB
+dbConnect()
+.then(() => console.log("Successfully connected to the DB!"))
+.catch(err => console.log("Error while connecting in DB: " + err));
+
 // routing
 app.use(routes);
 
 // port listing
-
 app.listen(PORT, () => console.log(`Server is running on ${PORT}...`));
