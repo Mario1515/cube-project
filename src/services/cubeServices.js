bbd08b0b59979175905f58dd46404d412e7f213d
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-09-29 16:47:43 +0300

    CUB-P2
    CUB-P2 / implement getSingleCube, getAll with cube model, fix template's ID to underscore

--- src/services/cubeServices.js
+++ src/services/cubeServices.js
@@ -7,9 +7,10 @@ exports.create = async (cubeData) => {
     return cube;
 }
 
-exports.getAll = (search, from, to) => {
-    let filterCubes = [...cubes];
+exports.getAll = async (search, from, to) => {
+    let filterCubes = await Cube.find().lean();
 
+    // this will be filtered later with mongoose
     if(search){
         filterCubes = filterCubes.filter((cube) => 
         cube.name.toLowerCase().includes(search.toLowerCase()));
@@ -28,6 +29,4 @@ exports.getAll = (search, from, to) => {
     return filterCubes;
 }
 
-exports.getSingleCube = (id) => {
-    return cubes.find(cube => cube.id === id);
-}
+exports.getSingleCube = (id) => Cube.findById(id);
