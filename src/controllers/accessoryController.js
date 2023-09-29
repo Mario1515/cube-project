a81b7f43c6c6a6731a577c3d27cd418b07377d8c
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-09-29 17:26:21 +0300

    CUB-P2
    CUB-P2 / added accessory endpoint and service

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
+const accessorySevice = require("./../services/accessoryService");
 
 router.get("/create", (req, res) => {
     res.render("accessories/create");
-})
+});
+
+router.post("/create", async (req, res) => {
+    const { name, description, imageUrl } = req.body;
+
+    await accessorySevice.create({name, description, imageUrl});
+
+    res.redirect("/");
+});
 
 module.exports = router;
